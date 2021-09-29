a85fadcc1eb459d8ae9449da6027aca63d1bc7f3
Anelisa Rodrigues <anelisa.srodrigues@gmail.com>
2021-09-29 12:54:43 -0300

    main

---
+++ teste.java
@@ -0,0 +1,21 @@
+public class PratoPrincipal
+{
+    public int CdProduto;
+    public string Descricao;
+    public double Preco;
+
+    public Item(int cdproduto, string descr, double prc)
+    {
+        CdProduto = cdproduto;
+        Descricao = descr;
+        Preco = prc;
+    }
+}
+
+public class Venda
+{
+    public static void main (string args[])
+    {
+        
+    }
+}
